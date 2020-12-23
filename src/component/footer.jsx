3c17cea162ce3828e11e64badea344cf399d7731
sushantshpatil <sushantshpatil@outlook.com>
2020-12-23 22:15:55 +0530

    initial commit

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import codepen from "../img/codepen.png";
 import github from "../img/github.png";
 import linkedin from "../img/linkedin.png";
 
@@ -13,13 +11,13 @@ class Footer extends React.Component {
 						<div className="col-12 col-sm-12 col-md-8 mx-auto">
 							<a
 								className="px-3"
-								href="https://github.com/kodi24fever"
+								href="https://github.com/sushantshpatil"
 								target="_blank"
 								rel="noopener noreferrer">
 								<img src={github} alt=""/>
 							</a>
 							<a
-								href="https://www.linkedin.com/in/frankizquierdo"
+								href="https://www.linkedin.com/in/sushantshpatil"
 								target="_blank"
 								rel="noopener noreferrer">
 								<img src={linkedin} alt=""/>
